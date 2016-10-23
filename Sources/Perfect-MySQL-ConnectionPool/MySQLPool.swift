725a5f784645a61bdf2b776fddcc3320259736ec
Ubaldo Cotta <ucotta@me.com>
2016-10-23 23:33:11 +0300

    preparing project

---
+++ Sources/Perfect-MySQL-ConnectionPool/MySQLPool.swift
@@ -0,0 +1,4 @@
+struct Perfect_MySQL_ConnectionPool {
+
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import Perfect_MySQL_ConnectionPoolTests
+
+XCTMain([
+     testCase(Perfect_MySQL_ConnectionPoolTests.allTests),
+])
