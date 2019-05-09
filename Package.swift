437e80e47d3a69d7c2f94e4f3184a6f3100125ff
Rockford Wei <rocky@perfect.org>
2019-05-09 10:28:10 -0400

    normalizing package name.

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "MySQL-ConnectionPool",
+    name: "MySQLConnectionPool",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "MySQL-ConnectionPool",
-            targets: ["MySQL-ConnectionPool"]),
+            name: "MySQLConnectionPool",
+            targets: ["MySQLConnectionPool"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,10 +21,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "MySQL-ConnectionPool",
+            name: "MySQLConnectionPool",
             dependencies: ["PerfectThread", "PerfectMySQL"]),
         .testTarget(
             name: "MySQLConnectionPoolTests",
-            dependencies: ["MySQL-ConnectionPool"]),
+            dependencies: ["MySQLConnectionPool"]),
     ]
 )
